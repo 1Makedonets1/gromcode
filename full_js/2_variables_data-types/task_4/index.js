6b6362934a87d033cd5ead0263d7b32842608c31
__ALEKSANDR__ <akairets89@gmail.com>
2021-11-17 11:11:46 +0200

    add live coding less4

---
+++ full_js/2_variables_data-types/task_4/index.js
@@ -0,0 +1,9 @@
+const a = 4;
+const b = 2;
+
+console.log('a:', a)
+console.log('b: значение переменной', b);
+console.log('a - b =', a - b);
+console.log('a + b =', a + b);
+console.log('a / b =', a / b);
+console.log('a * b =', a * b);
---
+++ live_coding/less_4/index.js
@@ -0,0 +1,19 @@
+// How to work on tech tasks. Step by step guide
+
+// 1. Learn requirement
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Write draft solution & testing
+// 4. Refactoring & final testing -> final solution
+
+//algo
+// 1. iterate numbers from 2 to num
+// 2. iterate from 2 to i
+// 3. если остаток от деления i на num равно 0 - то не простое
+// 4. если на все числа деление с остатком - то число простое
+// вывести в консоль
+
+//input: num
+//output: underfined
+function getPrimes(num) {
+  // put your code here
+}
